f7052e369cd8bd8c536f7bb11c1539c0292ad987
mcheng3 <michael.cheng65@gmail.com>
2017-09-14 14:46:53 -0400

    Update occupation.py

--- occupation.py
+++ occupation.py
@@ -1,1 +1,7 @@
-a
+source = open('occupations.csv', 'r')
+file = source.read()
+print(file)
+occupationsList = file.split('\r\n')
+print(occupationsList)
+occupationsDict = {}
+print(occupationsDict)
