d34c99648e139a58e8c5a91650f6c9c74f22ea89
mcheng3 <michael.cheng65@gmail.com>
2017-09-15 14:37:50 -0400

    Finish dictionary

--- occupation.py
+++ occupation.py
@@ -2,6 +2,36 @@ source = open('occupations.csv', 'r')
 file = source.read()
 print(file)
 occupationsList = file.split('\r\n')
+occupationsList.remove('')
+occupationsList.remove('Job Class,Percentage')
 print(occupationsList)
 occupationsDict = {}
 print(occupationsDict)
+tempList = []
+for x in occupationsList:
+    if(x.find('''",''') != -1):
+        tempList = x.split('''",''')
+        tempList.insert(0, tempList.pop(0) + '''"''')
+        print(tempList)
+        occupationsDict[tempList[0]] = float(tempList[1])
+    else:
+        tempList = x.split(",")
+        print(tempList)
+        occupationsDict[tempList[0]] = float(tempList[1])
+
+print(occupationsDict)
+
+def pickOccupation():
+    blah = 0
+
+
+
+
+
+        
+
+
+
+
+        
+        
