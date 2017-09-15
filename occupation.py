6e22c5cdee7c0eaa5b22903f8c0be39cdfb7d8b7
mcheng3 <michael.cheng65@gmail.com>
2017-09-15 14:46:51 -0400

    Update occupation.py

--- occupation.py
+++ occupation.py
@@ -7,35 +7,34 @@ occupationsList = file.split('\r\n')
 occupationsList.remove('')
 occupationsList.remove('Job Class,Percentage')
 occupationsList.remove('Total,99.8')
-print(occupationsList)
+#print(occupationsList)
 occupationsDict = {}
-print(occupationsDict)
+#print(occupationsDict)
 tempList = []
 for x in occupationsList:
     if(x.find('''",''') != -1):
         tempList = x.split('''",''')
         tempList.insert(0, tempList.pop(0) + '''"''')
-        print(tempList)
+        #print(tempList)
         occupationsDict[tempList[0]] = float(tempList[1])
     else:
         tempList = x.split(",")
-        print(tempList)
+        #print(tempList)
         occupationsDict[tempList[0]] = float(tempList[1])
 
-print(occupationsDict)
+#print(occupationsDict)
 
 bigList = []
 
 def pickOccupation():
     blah = 0
     for k, v in occupationsDict.items():
-        print(k)
-        print(v)
+        #print(print(v)
         counter = 0;
         while(counter < v * 10):
             bigList.append(k)
             counter += 1
-    print(bigList)
+    print(random.choice(bigList))
         
 
 pickOccupation()
